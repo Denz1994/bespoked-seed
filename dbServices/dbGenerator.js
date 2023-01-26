49890258d98c45c3a51fc3a754eee981c6016d4b
denz1994 <denz1994@gmail.com>
2023-01-26 10:33:12 -0500

    Built seeding generator for Products table.

--- dbServices/dbGenerator.js
+++ dbServices/dbGenerator.js
@@ -1,22 +1,47 @@
 const dpConnector = require('./dbConnector');
+const {faker} = require('@faker-js/faker');
 
 ROWS = 10;
 
 const seedProducts = async() => {
     for(i = 0; i < ROWS; i++){
+        manufacturer = faker.company.name();
+        productName = faker.commerce.product();
+
+        maxPurchase = 10000;
+        purchasePrice = faker.datatype.float({min:10, max:maxPurchase, percision:2});
+        salesPrice = faker.datatype.float({min:purchasePrice, max:maxPurchase*2, percision:2});
+        qtyOnHand = faker.datatype.number({min:1, max: 100});
+        commissionPercentage = faker.datatype.number({min:1, max:30, precision:1}) * .01;
+        
         await dpConnector.query(`
-        INSERT INTO Products (Manufacturer) VALUES ('Nestle')
+        INSERT INTO Products (
+            Manufacturer,
+            Product_Name,
+            Purchase_Price,
+            Sale_Price,
+            Qty_On_Hand,
+            Commission_Percentage
+        )
+        VALUES (
+            "${manufacturer}",
+            "${productName}",
+            "${purchasePrice}",
+            "${salesPrice}",
+            "${qtyOnHand}",
+            "${commissionPercentage}"
+        );
         `);
     }
 }
 
-const seedProductsTable = async(req,res) =>{    
+const seedProductsTable = async() =>{    
     try {
         const dropTableQuery = `DROP TABLE IF EXISTS Products`;
         const createTableQuery = `CREATE TABLE Products (
             ID INT UNIQUE PRIMARY KEY AUTO_INCREMENT,
             Manufacturer TEXT,
-            Style TEXT,
+            Product_Name TEXT,
             Purchase_Price FLOAT,
             Sale_Price FLOAT,
             Qty_On_Hand INT,
@@ -31,5 +56,4 @@ const seedProductsTable = async(req,res) =>{
         console.error(err.message);
     }
 }
-
 seedProductsTable();
